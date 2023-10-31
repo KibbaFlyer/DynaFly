a2d626abc7f88f6e8e795c6980865a07b3cf78e8
KibbaFlyer <kristoffer.flygare@icloud.com>
2023-10-31 11:21:56 +0100

    Adding Template nodes

--- DynaFly/Revit.cs
+++ DynaFly/Revit.cs
@@ -5,15 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RevitServices.Persistence;
+using Revit.Elements;
+using Revit.Elements.Views;
+using System.Collections.ObjectModel;
 
 namespace Revit
 {
     /// <summary>
-    /// Contains nodes for interacting with Phases
+    /// Contains nodes for interacting with Revit Elements
     /// </summary>
-    public class Elements
+    public class ElementData
     {
-        private Elements() { }
+        private ElementData() { }
         /// <summary>
         /// Gets a list of all phases in the current Revit document.
         /// Is different from the OOTB-node in that it returns Elements of type Autodesk.Revit.DB.Phase instead of Revit.Elements.Phase
@@ -25,10 +28,10 @@ namespace Revit
             Document doc = DocumentManager.Instance.CurrentDBDocument;
             // Collecting all the phases in the document
             FilteredElementCollector collector = new FilteredElementCollector(doc);
-            ICollection<Element> phaseElements = collector.OfClass(typeof(Phase)).ToElements();
+            ICollection<Autodesk.Revit.DB.Element> phaseElements = collector.OfClass(typeof(Phase)).ToElements();
 
             var phases = new List<Phase>();
-            foreach (Element element in phaseElements)
+            foreach (Autodesk.Revit.DB.Element element in phaseElements)
             {
                 if (element is Phase phase)
                 {
@@ -37,6 +40,53 @@ namespace Revit
             }
 
             return phases;
+        }
+        /// <summary>
+        /// Looks for all view templates in the project.
+        /// DOES NOT INCLUDE 3D VIEW TEMPLATES
+        /// There is a missing functionality in Dynamo to cast Revit API View3D to Dynamo views
+        /// </summary>
+        /// <returns>A list of view templates</returns>
+        public static List<Revit.Elements.Element> GetAllViewTemplates ()
+        {
+            Document doc = DocumentManager.Instance.CurrentDBDocument;
+            FilteredElementCollector collector = new FilteredElementCollector(doc)
+                .WhereElementIsNotElementType()
+                .OfClass(typeof(Autodesk.Revit.DB.View));
+
+            var viewTemplates = new List<Revit.Elements.Element>();
+            foreach (Autodesk.Revit.DB.View view in collector)
+            {
+                // Check if the view's OwnerViewId is InvalidElementId, which means it's a template
+                if (view.IsTemplate)
+                {
+                    var dynamoView = ElementWrapper.ToDSType(view, true) as Revit.Elements.Element;
+                    if(dynamoView != null)
+                    {
+                        viewTemplates.Add(dynamoView);
+                    }
+                }
+            }
+            return viewTemplates;
+        }
+        /// <summary>
+        /// Looks for all USED view templates in the project
+        /// </summary>
+        /// <returns>A list of view templates</returns>
+        public static List<Revit.Elements.Views.View> GetAllUsedViewTemplates()
+        {
+            Document doc = DocumentManager.Instance.CurrentDBDocument;
+
+                // Fetch all views in the project
+                FilteredElementCollector viewCollector = new FilteredElementCollector(doc).OfClass(typeof(Autodesk.Revit.DB.View));
+
+                // Use LINQ to filter and transform the collection
+                List<Revit.Elements.Views.View> usedViewTemplates = viewCollector.Cast<Autodesk.Revit.DB.View>()
+                    .Where(view => !view.IsTemplate && view.ViewTemplateId != ElementId.InvalidElementId && view.ViewTemplateId.IntegerValue != -1)
+                    .Select(view => ElementWrapper.ToDSType(view, true) as Revit.Elements.Views.View)
+                    .ToList();
+
+            return usedViewTemplates;
         }
     }
 }
